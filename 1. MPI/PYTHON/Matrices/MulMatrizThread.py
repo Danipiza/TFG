0abf4bff1a77bd4472dacdb5447f8471ee1e0c6a
DannyP39 <danipiza@ucm.es>
2024-04-23 15:39:40 +0200

    Thread?

--- 1. MPI/PYTHON/Matrices/MulMatrizThread.py
+++ 1. MPI/PYTHON/Matrices/MulMatrizThread.py
@@ -1,7 +1,32 @@
 #from mpi4py import MPI
-import time
+
 import sys
 import os
+import time
+
+"""
+Normal:
+M100X100    =    0.15625s
+M1000X1000  =    196.84375s
+"""
+
+import threading
+"""
+medido por time
+
+M100X100   =    0.125s
+M1000X1000 =    125.84375s
+"""
+
+import multiprocessing
+"""
+medido por time
+
+M100X100   =    0.046875s
+M1000X1000 =    56.25875s
+"""
+
+from concurrent.futures import ThreadPoolExecutor
 
 
 
@@ -11,6 +36,53 @@ def print_matriz(matriz):
         print("Fila {}: {}".format(i, x))
         i+=1
 
+# Funcion para cada thread
+def multiply_row(id, matrizA, matrizB, matrizC, n,m, rows):    
+    for row in range(rows[0],rows[1]):
+        for col in range(m):
+            tmp=0
+            for k in range(n):
+                tmp+=matrizA[row][k]*matrizB[k][col]
+            matrizC[row][col] = tmp
+    print("Termina",id)
+
+
+
+def create_threads(numThreads, matrizA, matrizB, matrizC):
+    threads = []
+
+    asig=[]
+
+    n=len(matrizA)
+    m=len(matrizB)
+    tam=n//numThreads
+    mod=n%numThreads
+    izq=0
+    for i in range(mod):
+        asig.append([izq,izq+tam+1])
+        izq+=tam+1
+
+    for i in range(mod,numThreads):
+        asig.append([izq,izq+tam])
+        izq+=tam
+
+    for i in range(numThreads):
+        # print("MASTER",i)
+        """thread = threading.Thread(target=multiply_row, args=(i, matrizA, matrizB, matrizC, n,m, asig[i]))
+        threads.append(thread)
+        thread.start()"""
+
+        process = multiprocessing.Process(target=multiply_row, args=(i, matrizA, matrizB, matrizC, n,m, asig[i]))
+        threads.append(process)
+        process.start()
+
+    """with ThreadPoolExecutor(max_workers=5) as executor:
+        for i in range(5):
+            executor.submit(multiply_row, args=(i, matrizA, matrizB, matrizC, n,m, asig[i]))"""
+
+    # Espera a que todos terminen 
+    for thread in threads:
+        thread.join()
 
 
 def main():       
@@ -28,23 +100,26 @@ def main():
     else:
         matrizC = [[0 for _ in range(cB)] for _ in range(fA)]
     
-    #timeStart = MPI.Wtime()
+    
+    
     timeStart = time.process_time()
+    #timeStart = MPI.Wtime()
+
+    create_threads(4,matrizA, matrizB, matrizC)
     
-    for i in range(fA):
-        for j in range(cB):
-            for k in range(cA):
-                matrizC[i][j]+=matrizA[i][k]*matrizB[k][j]
-    
-    #timeEnd = MPI.Wtime()
     timeEnd = time.process_time()
+    #timeEnd = MPI.Wtime()
+
     print("Tiempo de ejecucion: {}s".format(timeEnd-timeStart))
     
-    if PRINT: print(matrizC)
+    if PRINT: 
+        for f in matrizC:
+            print(f)
         
     
 
     
+
 
 def leeArchivo(archivo):
     """
@@ -86,5 +161,5 @@ def leeArchivo(archivo):
     
     return M, filas, columnas
     
-
-main()
+if __name__ == "__main__":
+    main()
